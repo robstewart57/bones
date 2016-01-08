90eb5bf100b4d1df78b729c5792f14e0ca051252
Blair Archibald <mrblairarchibald@gmail.com>
2016-01-08 11:47:08 +0000

    Added simple HdpH broadcast B&B skeleton
    This includes the definition of a new data type for HdpH based branch
    and bound functions (unfortunately due to Closures I can't easily unify
    this with a pure version).

--- src/Bones/Skeletons/BranchAndBound/HdpH/Broadcast.hs
+++ src/Bones/Skeletons/BranchAndBound/HdpH/Broadcast.hs
@@ -1,6 +1,234 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Bones.Skeletons.BranchAndBound.HdpH.Broadcast
   (
-    search
+    declareStatic
+  , search
   ) where
 
-search = undefined
+import           Control.Parallel.HdpH (Closure, Node, Par, StaticDecl,
+                                        StaticToClosure, Thunk (Thunk),
+                                        ToClosure, allNodes, declare, get, here,
+                                        io, locToClosure, mkClosure, myNode,
+                                        one, pushTo, spawn, spawnAt, static,
+                                        staticToClosure, toClosure, unClosure)
+
+import           Control.Monad         (forM_, when)
+
+import           Data.IORef            (IORef, atomicModifyIORef',
+                                        atomicWriteIORef, newIORef, readIORef)
+import qualified Data.Map.Strict       as Map (Map, empty, insert, lookup)
+import           Data.Monoid           (mconcat)
+
+import           System.IO.Unsafe      (unsafePerformIO)
+
+--------------------------------------------------------------------------------
+--- Registry Functionality for global variables
+--------------------------------------------------------------------------------
+
+searchSpaceKey :: Int
+{-# INLINE searchSpaceKey #-}
+searchSpaceKey = 0
+
+solutionKey :: Int
+{-# INLINE solutionKey #-}
+solutionKey = 1
+
+boundKey :: Int
+{-# INLINE boundKey #-}
+boundKey = 2
+
+registry :: IORef (Map.Map Int (IORef a))
+{-# NOINLINE registry #-}
+registry = unsafePerformIO $ newIORef Map.empty
+
+addGlobalSearchSpaceToRegistry :: IORef a -> IO ()
+addGlobalSearchSpaceToRegistry ref = do
+    reg      <- readIORef registry
+    let reg' = Map.insert searchSpaceKey ref reg
+    atomicWriteIORef registry reg'
+
+getRefFromRegistry :: Int -> IO (IORef a)
+getRefFromRegistry k = do
+  r <- readIORef registry
+  case Map.lookup k r of
+    Nothing -> error $ " Could not find key: " ++ show k ++ " in global registry."
+    Just x  -> return x
+
+readFromRegistry :: Int -> IO a
+readFromRegistry k = do
+  ref <- getRefFromRegistry k
+  readIORef ref
+
+initRegistryBound :: Closure a -> Thunk (Par ())
+initRegistryBound bnd = Thunk $ io $ do
+    bndRef <- newIORef $ bnd
+    reg    <- readIORef registry
+    let reg' = Map.insert boundKey bndRef reg
+    atomicWriteIORef registry reg'
+
+--------------------------------------------------------------------------------
+--- Data Types
+--------------------------------------------------------------------------------
+
+-- Functions required to specify a B&B computation
+data BAndBFunctions a b c s =
+  BAndBFunctions
+    { generateChoices :: Closure a -> Closure s -> Par [Closure c]
+    , shouldPrune     :: Closure c -> Closure a -> Closure b -> Bool
+    , updateBound     :: Closure b -> Closure b -> Bool
+    , step            :: Closure c -> Closure a -> Closure s
+                          -> Par (Closure a, Closure b, Closure s)
+    , removeChoice    :: Closure c -> Closure s-> Closure s
+    }
+
+instance ToClosure () where
+  locToClosure = $(here)
+
+--------------------------------------------------------------------------------
+--- Skeleton Functionality
+--------------------------------------------------------------------------------
+
+search :: Closure a
+       -> Closure s
+       -> Closure b
+       -> Closure (BAndBFunctions a b c s)
+       -> Par a
+search startingSol space bnd fs' = do
+  master <- myNode
+  ns     <- allNodes
+
+  initRegistry
+  initLocalRegistries ns
+
+  searchSpace <- io $ readFromRegistry searchSpaceKey
+
+  let fs = unClosure fs'
+
+  -- Gen at top level
+  ts   <- generateChoices fs searchSpace space
+
+  -- Generating the starting tasks remembering to remove choices from their left
+  -- from the starting "remaining" set
+  let tasks = let sr = tail $ scanl (flip (removeChoice fs)) space ts
+              in  zipWith (createChildren master) sr ts
+
+  children <- mapM (spawn one) (reverse tasks)
+  mapM get children
+
+  io $ unClosure . fst <$> readFromRegistry solutionKey
+    where
+      initRegistry = io $ do
+        solRef <- newIORef (startingSol, bnd)
+        reg    <- readIORef registry
+        let reg' = Map.insert solutionKey solRef reg
+        atomicWriteIORef registry reg'
+
+      initLocalRegistries nodes = do
+        forM_ nodes $ \n -> pushTo $(mkClosure [| initRegistryBound bnd |]) n
+
+      createChildren m rem c =
+        $(mkClosure [| branchAndBoundChild (m, c, startingSol, rem, fs') |])
+
+branchAndBoundChild ::
+    ( Node
+    , Closure c
+    , Closure a
+    , Closure s
+    , Closure (BAndBFunctions a b c s)
+    )
+    -> Thunk (Par (Closure ()))
+branchAndBoundChild (n, c, sol, rem, fs') =
+  Thunk $ do
+    let fs = unClosure fs'
+
+    bnd <- io $ readFromRegistry boundKey
+    if shouldPrune fs c sol bnd then
+        return $ toClosure ()
+    else do
+        (startingSol, _, rem') <- step fs c sol rem
+        toClosure <$> branchAndBoundExpand n startingSol rem' fs'
+
+branchAndBoundExpand ::
+       Node
+    -> Closure a
+    -> Closure s
+    -> Closure (BAndBFunctions a b c s)
+    -> Par ()
+branchAndBoundExpand parent sol rem fs' = do
+  let fs  = unClosure fs'
+
+  choices <- generateChoices fs sol rem
+
+  go sol rem choices fs
+    where go sol remaining [] fs      = return ()
+          go sol remaining (c:cs) fs  = do
+            bnd <- io $ readFromRegistry boundKey
+
+            if shouldPrune fs c sol bnd then
+              return ()
+            else do
+              (newSol, newBnd, remaining') <- step fs c sol remaining
+
+              when (updateBound fs newBnd bnd) $ do
+                 bAndb_parUpdateLocalBounds newBnd fs'
+                 bAndb_notifyParentOfNewBest parent (newSol, newBnd) fs'
+
+              branchAndBoundExpand parent newSol remaining' fs'
+
+              let remaining'' = removeChoice fs c remaining
+              go sol remaining'' cs fs
+
+bAndb_parUpdateLocalBounds :: Closure b
+                           -> Closure (BAndBFunctions a b c s)
+                           -> Par ()
+bAndb_parUpdateLocalBounds bnd fs = do
+  ref <- io $ getRefFromRegistry boundKey
+  io $ atomicModifyIORef' ref $ \b ->
+    if updateBound (unClosure fs) bnd b
+      then (bnd, ())
+      else (b, ())
+
+bAndb_updateLocalBounds :: (Closure b,
+                            Closure (BAndBFunctions a b c s))
+                        -> Thunk (Par ())
+bAndb_updateLocalBounds (bnd, fs) =
+  Thunk $ bAndb_parUpdateLocalBounds bnd fs
+
+bAndb_notifyParentOfNewBest :: Node
+                            -> (Closure a, Closure b)
+                            -> Closure (BAndBFunctions a b c s)
+                            -> Par ()
+bAndb_notifyParentOfNewBest parent solPlusBnd fs = do
+  -- Wait for an update ack to avoid a race condition in the case when all
+  -- children finish before the final updateBest task has ran on master.
+  spawnAt parent updateFn  >>= get
+  return ()
+
+  where updateFn = $(mkClosure [| bAndb_updateParentBest (solPlusBnd, fs) |])
+
+bAndb_updateParentBest :: ( (Closure a, Closure b)
+                          , Closure (BAndBFunctions a b c s))
+                       -> Thunk (Par (Closure ()))
+bAndb_updateParentBest ((sol, bnd), fs) = Thunk $ do
+  ref <- io $ getRefFromRegistry solutionKey
+  updated <- io $ atomicModifyIORef' ref $ \prev@(oldSol, b) ->
+                if updateBound (unClosure fs) bnd b
+                    then ((sol, bnd), True)
+                    else (prev      , False)
+
+  when updated $ do
+    ns <- allNodes
+    mapM_ (pushTo $(mkClosure [| bAndb_updateLocalBounds (bnd, fs) |])) ns
+
+  return $ toClosure ()
+
+$(return []) -- TH Workaround
+declareStatic :: StaticDecl
+declareStatic = mconcat
+  [
+    declare $(static 'initRegistryBound)
+  , declare $(static 'branchAndBoundChild)
+  , declare $(static 'bAndb_updateParentBest)
+  , declare $(static 'bAndb_updateLocalBounds)
+  , declare (staticToClosure :: StaticToClosure ())
+  ]
