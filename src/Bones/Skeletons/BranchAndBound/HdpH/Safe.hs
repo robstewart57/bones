fb358d0aad51a21684ba66fbc895fa97e8628b96
Blair Archibald <mrblairarchibald@gmail.com>
2016-02-11 10:40:30 +0000

    Adding dynamic work generation to the safeSkeleton

--- src/Bones/Skeletons/BranchAndBound/HdpH/Safe.hs
+++ src/Bones/Skeletons/BranchAndBound/HdpH/Safe.hs
@@ -4,6 +4,7 @@ module Bones.Skeletons.BranchAndBound.HdpH.Safe
   (
     declareStatic
   , search
+  , searchDynamic
   ) where
 
 import           Control.Parallel.HdpH (Closure, Node, Par, StaticDecl,
@@ -12,13 +13,16 @@ import           Control.Parallel.HdpH (Closure, Node, Par, StaticDecl,
                                         io, locToClosure, mkClosure, myNode,
                                         one, pushTo, spawn, spawnAt, static, spawnWithPrio,
                                         staticToClosure, toClosure, unClosure, probe,
-                                        put, new, glob, tryGet, tryRPut)
+                                        put, new, glob, tryGet, tryRPut, fork)
 
 import           Control.Monad         (forM_, forM, foldM, foldM_, when, replicateM, zipWithM)
+-- import           Control.Concurrent.Chan
+import           Control.Concurrent.STM (atomically)
+import           Control.Concurrent.STM.TChan (TChan, newTChanIO, writeTChan, readTChan)
 
 import           Data.IORef            (IORef, atomicModifyIORef')
 
-import           Data.Maybe            (catMaybes)
+import           Data.Maybe            (catMaybes, mapMaybe)
 import           Data.Monoid           (mconcat)
 
 import           Bones.Skeletons.BranchAndBound.HdpH.Types
@@ -88,7 +92,9 @@ search spawnDepth startingSol space bnd fs = do
 
       spawnTasksWithPrios (p, lst) (taken, task, c, sol, rem) =
         spawnWithPrio one p task >>= \res -> return (p + 1, (taken, res, c, sol, rem):lst)
+  
 
+-- Probably really want [Par a] not Par [a]. How can I get this?
 spawnAtDepth ::
               [(Closure c, Closure a, Closure s)]
            -> Node
@@ -96,9 +102,13 @@ spawnAtDepth ::
            -> Closure (BAndBFunctions a b c s)
            -> Par [(IVar (Closure ()), Closure (Par (Closure())), Closure c, Closure a, Closure s)]
 spawnAtDepth ts master curDepth fs =
+  -- This seems to be getting forced early. I want to keep this lazy if possible!
   if curDepth == 0
     then
         forM ts $ \(c,s,r) -> do
+          -- Check pruning here as well? (It gets checked in the task but we may
+          -- as well prune earlier if possible - this happens in another thread
+          -- so it's not a big overhead?)
           l <- new
           g <- glob l
 
@@ -118,6 +128,9 @@ spawnAtDepth ts master curDepth fs =
       step     <- catMaybes <$> mapM stepSol ts
       -- Then we generate choices form this step
       newLevel <- mapM constructChoices step
+
+      -- This might be forcing the list. I don't want this - how do I fix it? No idea yet.
+
       -- and recurse
       concat <$> mapM (\ts' -> spawnAtDepth ts' master (curDepth - 1) fs) newLevel
 
@@ -129,9 +142,10 @@ spawnAtDepth ts master curDepth fs =
 
         bnd <- io $ readFromRegistry boundKey
 
+        io (putStrLn "stepingTask")
         -- Check if we can prune first to avoid any extra work
         if (unClosure $ shouldPrune fs') c sol bnd
-          then return Nothing
+          then io (putStrLn "Pruning") >> return Nothing
           else do
             (sol', _, remaining') <- (unClosure $ step fs') c sol remaining
             return $ Just (sol', remaining')
@@ -269,6 +283,170 @@ bAndb_updateParentBest ((sol, bnd), fs) = Thunk $ do
     mapM_ (pushTo $(mkClosure [| bAndb_updateLocalBounds (bnd, fs) |])) ns
 
   return $ toClosure ()
+
+-- Skeleton which attempts to keep "activeTasks" tasks available without
+-- generating all work ahead of time
+searchDynamic :: Int
+              -> Int
+              -> Closure a
+              -> Closure s
+              -> Closure b
+              -> Closure (BAndBFunctions a b c s)
+              -> Par a
+searchDynamic activeTasks spawnDepth startingSol space bnd fs = do
+  let fs' = unClosure fs
+
+  allNodes >>= initRegistries
+
+  -- Get top level choices
+  searchSpace <- io $ readFromRegistry searchSpaceKey
+  cs <- (unClosure $ generateChoices fs') searchSpace space
+
+  -- Sequentially execute tasks up to depth d.
+  myNode >>= spawnAndExecuteTasks cs fs'
+
+  -- Return the result stored in the global bound as the answer
+  io $ unClosure . fst <$> readFromRegistry solutionKey
+    where
+      initRegistries nodes = do
+        io $ addToRegistry solutionKey (startingSol, bnd)
+        forM_ nodes $ \n -> pushTo $(mkClosure [| initRegistryBound bnd |]) n
+
+      spawnAndExecuteTasks choices fs' master  = do
+
+        let spaces = tail $ scanl (flip (unClosure $ removeChoice fs')) space choices
+            topLevelChoices = zip3 choices (replicate (length spaces) startingSol) spaces
+
+        -- Channels may block the scheduler, issue with scheds=1
+        taskQueue <- io newTChanIO
+
+        fork $ taskGenerator topLevelChoices master spawnDepth fs taskQueue activeTasks
+
+        handleTasks master taskQueue
+
+      handleTasks m tq = do
+        t <- io . atomically $ readTChan tq
+        case t of
+          Task (taken, res, c, sol, rem) -> do
+            wasTaken <- probe taken
+            if wasTaken
+            then spinGet res >> handleTasks m tq
+            else do
+              put taken (toClosure ())
+              safeBranchAndBoundSkeletonChild (c , m, sol , rem , fs)
+              handleTasks m tq
+          Done -> return $ toClosure ()
+
+data Task a = Task a | Done deriving (Show)
+
+taskGenerator ::
+     [(Closure c, Closure a, Closure s)]
+  -> Node
+  -> Int
+  -> Closure (BAndBFunctions a b c s)
+  -> TChan (Task (IVar (Closure ()), IVar (Closure ()), Closure c, Closure a, Closure s))
+  -> Int
+  -> Par ()
+taskGenerator toplvl m depth fs tq n = do
+  -- Keep generating tasks such that we keep N tasks active if possible
+  -- Idea is to just call spawnAtDepthLazy and let lazyness do it's thing
+
+   -- Take N to start with
+  ts <- spawnAtDepthLazy toplvl m depth fs
+  let startingTasks = take n ts
+      remainingTasks = drop n ts
+
+  st <- catMaybes <$> sequence startingTasks
+
+  (nextP, signals, tasks) <- foldM spawnTasksWithPrios (1, [], []) st
+
+  -- Give the tasks to the master
+  mapM_ (io . atomically . writeTChan tq . Task) tasks
+
+  generateNewTasks remainingTasks signals nextP
+
+  where
+      spawnTasksWithPrios (p, ts, lst) (taken, task, c, sol, rem) =
+        spawnWithPrio one p task >>= \res -> return (p + 1, taken:ts, (taken, res, c, sol, rem):lst)
+
+      spawnTaskWithPrio p (taken, task, c, sol, rem) =
+        spawnWithPrio one p task >>= \res -> return (p + 1, taken, (taken, res, c, sol, rem))
+
+      generateNewTasks taskList takenSignals prio = do
+
+        signalList <- getAny takenSignals
+
+        -- SpawnNewTask always gets a task when one is available
+        (task, tl) <- spawnNewTask taskList 
+        case task o
+  f
+          Just t -> do
+            (prio', s, t) <- spawnTaskWithPrio prio t
+            io . atomically $ writeTChan tq (Task t) 
+            generateNewTasks tl (s:signalList) prio'
+          Nothing -> io . atomically $ writeTChan tq Done
+
+      spawnNewTask tl =
+        case tl of
+          (t:ts) -> do
+            t' <- t
+            case t' of
+              Just x   -> return (Just x, ts)
+              Nothing  -> spawnNewTask ts
+          [] -> return (Nothing, [])
+
+      -- Not sure how efficient this is due to the cyclic appending, it could be a bottleneck?
+      -- Zippers might be a better choice here
+      getAny [] = return []
+
+      getAny (x:xs)= do
+        filled <- probe x
+        if filled then return xs else getAny (xs ++ [x])
+
+
+spawnAtDepthLazy ::
+              [(Closure c, Closure a, Closure s)]
+           -> Node
+           -> Int
+           -> Closure (BAndBFunctions a b c s)
+           -> Par [Par (Maybe (IVar (Closure ()), Closure (Par (Closure())), Closure c, Closure a, Closure s))]
+spawnAtDepthLazy ts master curDepth fs
+  | curDepth == 0 = return $ map makeTask ts
+  | otherwise = do
+      step     <- catMaybes <$> mapM stepSol ts
+      newLevel <- mapM constructChoices step
+      concat <$> mapM (\ts' -> spawnAtDepthLazy ts' master (curDepth - 1) fs) newLevel
+  where
+       stepSol (c, sol, remaining) = do
+        let fs' = unClosure fs
+
+        (sol', _, remaining') <- (unClosure $ step fs') c sol remaining
+        return $ Just (sol', remaining')
+
+       constructChoices (sol, remaining) = do
+           let fs' = unClosure fs
+           cs <- (unClosure $ generateChoices fs') sol remaining
+
+           let spaces = tail $ scanl (flip (unClosure $ removeChoice fs')) remaining cs
+
+           return $ zip3 cs (replicate (length spaces) sol) spaces
+
+       makeTask (c, s, r) = do
+        bnd <- io $ readFromRegistry boundKey
+
+        -- Check if we can prune first to avoid extra work
+        let fs' = unClosure fs
+        if (unClosure $ shouldPrune fs') c s bnd
+          then return Nothing
+          else do
+          l <- new
+          g <- glob l
+
+          let task  =
+                $(mkClosure [| safeBranchAndBoundSkeletonChildTask ( g , c , master
+                                                                   , s , r , fs) |])
+
+          return $ Just (l, task, c, s, r)
 
 $(return []) -- TH Workaround
 declareStatic :: StaticDecl
