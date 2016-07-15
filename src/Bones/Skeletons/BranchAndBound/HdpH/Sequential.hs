e06953387dbe5218e1972616967dfdd91211e969
Blair Archibald <b.archibald.1@research.gla.ac.uk>
2016-07-15 07:44:51 +0100

    Move sequential skeleton to new API

--- src/Bones/Skeletons/BranchAndBound/HdpH/Sequential.hs
+++ src/Bones/Skeletons/BranchAndBound/HdpH/Sequential.hs
@@ -14,55 +14,46 @@ import Bones.Skeletons.BranchAndBound.HdpH.Types
 import Bones.Skeletons.BranchAndBound.HdpH.GlobalRegistry
 
 -- Assumes any global space state is already initialised
-search :: a -> s -> b -> BAndBFunctionsL a b c s -> Par a
-search ssol sspace sbnd fns = do
+search :: NodeBB a b s -> BAndBFunctionsL a b s -> Par a
+search root@(ssol, sbnd, _) fns = do
   io $ addToRegistry solutionKey (ssol, sbnd)
   io $ addToRegistry boundKey sbnd
-  expand ssol sspace fns
+  expand root fns
   io $ fst <$> readFromRegistry solutionKey
 
-expand :: a -> s -> BAndBFunctionsL a b c s -> Par ()
-expand sol space fns = go1 sol space
+expand :: NodeBB a b s -> BAndBFunctionsL a b s -> Par ()
+expand root fns = go1 root
   where
-    go1 s r = generateChoicesL fns s r >>= go s r
+    go1 n = orderedGenerator fns n >>= go
 
-    go _ _ [] = return ()
+    go [] = return ()
 
-    go sol remaining (c:cs) = do
+    go (n:ns) = do
       bnd <- io $ readFromRegistry boundKey
 
-      sp <- shouldPruneL fns c bnd sol remaining
+      sp <- pruningPredicateL fns n bnd
       case sp of
-        Prune      -> do
-          remaining'' <- removeChoiceL fns c remaining
-          go sol remaining'' cs
-
+        Prune      -> go sol ns cs
         PruneLevel -> return ()
-
         NoPrune    -> do
-          (newSol, newBnd, remaining') <- stepL fns c sol remaining
+         when (strengthenL fns n)
+            updateLocalBoundAndSol n fns
 
-          when (updateBoundL fns newBnd bnd) $
-              updateLocalBoundAndSol newSol newBnd fns
-
-          go1 newSol remaining'
-
-          remaining'' <- removeChoiceL fns c remaining
-          go sol remaining'' cs
+          go1 n >> go ns
 
 -- TODO: Technically we don't need atomic modify when we are sequential but this
 -- keeps us closer to the parallel version.
-updateLocalBoundAndSol :: a -> b -> BAndBFunctionsL a b c s -> Par ()
-updateLocalBoundAndSol sol bnd fns = do
+updateLocalBoundAndSol :: NodeBB a b s -> BAndBFunctionsL a b c s -> Par ()
+updateLocalBoundAndSol n@(sol, bnd, _) fns = do
   -- Bnd
   ref <- io $ getRefFromRegistry boundKey
   io $ atomicModifyIORef' ref $ \b ->
-    if updateBoundL fns bnd b then (bnd, ()) else (b, ())
+    if strengthen fns n b then (bnd, ()) else (b, ())
 
   -- Sol
   ref <- io $ getRefFromRegistry solutionKey
   io $ atomicModifyIORef' ref $ \prev@(_,b) ->
-        if updateBoundL fns bnd b
+        if strength fns n b
             then ((sol, bnd), True)
             else (prev, False)
 
