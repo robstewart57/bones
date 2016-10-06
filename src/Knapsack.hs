00cbe21c2d62af4684636af562ca9505e233995b
Blair Archibald <mrblairarchibald@gmail.com>
2016-10-06 12:27:33 +0100

    Migrate to the new API

--- src/Knapsack.hs
+++ src/Knapsack.hs
@@ -25,7 +25,7 @@ import qualified Bones.Skeletons.BranchAndBound.HdpH.Broadcast as Broadcast
 import qualified Bones.Skeletons.BranchAndBound.HdpH.Sequential as Sequential
 
 import Control.DeepSeq (NFData)
-import Control.Monad (when)
+import Control.Monad (when, foldM)
 
 import GHC.Generics (Generic)
 
@@ -47,51 +47,41 @@ skeletonSafe items capacity depth diversify =
   Safe.search
     diversify
     depth
-    (Solution (length items) capacity [] 0 0)
-    (map (\(a,b,c) -> a) items)
-    (0 :: Int)
+    (Solution (length items) capacity [] 0 0, 0, map (\(a,b,c) -> a) items)
     (toClosure (BAndBFunctions
-      $(mkClosure [| generateChoices |])
-      $(mkClosure [| shouldPrune |])
-      $(mkClosure [| shouldUpdateBound |])
-      $(mkClosure [| step |])
-      $(mkClosure [| removeChoice |])))
+      $(mkClosure [| orderedGenerator |])
+      $(mkClosure [| pruningPredicate|])
+      $(mkClosure [| strengthen |])))
     (toClosure (ToCFns
       $(mkClosure [| toClosureSolution |])
       $(mkClosure [| toClosureInt |])
-      $(mkClosure [| toClosureItem |])
-      $(mkClosure [| toClosureItemList |])))
+      $(mkClosure [| toClosureItemList |])
+      $(mkClosure [| toClosureKPNode |])))
 
 skeletonBroadcast :: [(Int, Int, Int)] -> Int -> Int -> Bool -> Par Solution
 skeletonBroadcast items capacity depth diversify =
   Broadcast.search
     depth
-    (Solution (length items) capacity [] 0 0)
-    (map (\(a,b,c) -> a) items)
-    (0 :: Int)
+    (Solution (length items) capacity [] 0 0, 0, map (\(a,b,c) -> a) items)
     (toClosure (BAndBFunctions
-      $(mkClosure [| generateChoices |])
-      $(mkClosure [| shouldPrune |])
-      $(mkClosure [| shouldUpdateBound |])
-      $(mkClosure [| step |])
-      $(mkClosure [| removeChoice |])))
+      $(mkClosure [| orderedGenerator |])
+      $(mkClosure [| pruningPredicate|])
+      $(mkClosure [| strengthen |])))
     (toClosure (ToCFns
       $(mkClosure [| toClosureSolution |])
       $(mkClosure [| toClosureInt |])
-      $(mkClosure [| toClosureItem |])
-      $(mkClosure [| toClosureItemList |])))
+      $(mkClosure [| toClosureItemList |])
+      $(mkClosure [| toClosureKPNode |])))
 
 skeletonSequential :: [(Int, Int, Int)] -> Int -> Par Solution
 skeletonSequential items capacity =
   Sequential.search
-    (Solution (length items) capacity [] 0 0)
-    (map (\(a,b,c) -> a) items)
-    (0 :: Int)
-    (BAndBFunctionsL generateChoices shouldPrune shouldUpdateBound step removeChoice)
+    (Solution (length items) capacity [] 0 0, 0, map (\(a,b,c) -> a) items)
+    (BAndBFunctionsL orderedGenerator pruningPredicate strengthen)
 
---------------------------------------------------------------------------------
--- An inlined version of the sequential skeleton
---------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------
+-- -- An inlined version of the sequential skeleton
+-- --------------------------------------------------------------------------------
 sequentialInlined :: [(Int, Int, Int)] -> Int -> Par Solution
 sequentialInlined items capacity =
   seqSearch (Solution (length items) capacity [] 0 0) (map (\(a,b,c) -> a) items) 0
@@ -166,6 +156,7 @@ updateLocalBoundAndSol sol bnd = do
 --  removeChoice    :: Closure (Closure c -> Closure s-> Closure s)
 
 -- Potential choices is simply the list of un-chosen items
+
 generateChoices :: Solution -> [Item] -> Par [Item]
 generateChoices (Solution _ cap _ _ curWeight) remaining = do
   (_ , weights) <- io (getGlobalSearchSpace :: IO (Array Int Int, Array Int Int))
@@ -210,12 +201,52 @@ removeChoice :: Item -> [Item] -> Par [Item]
 removeChoice i its = return $ delete i its
 
 --------------------------------------------------------------------------------
+-- Skeleton Functions
+--------------------------------------------------------------------------------
+
+type KPNode = (Solution, Int, [Int])
+
+orderedGenerator :: KPNode -> Par [Par KPNode]
+orderedGenerator ((Solution mix cap is solP solW), bnd, remaining) = do
+  (profits, weights) <- io (getGlobalSearchSpace :: IO (Array Int Int, Array Int Int))
+  let items = filter (\i -> solW + fromIntegral (weights ! i) <= cap) remaining
+
+  return $ map (\i -> return (Solution mix cap (i:is) (solP + (profits ! i)) (solW + (weights ! i)),
+                              solP + (profits ! i),
+                              delete i remaining)) items
+
+pruningPredicate :: KPNode -> Int -> Par PruneType
+pruningPredicate ((Solution mix cap (i:is) solP solW), _, _) bnd = do
+  (profits, weights) <- io getGlobalSearchSpace
+  -- Profits/weights already in solution for i so don't add them again does this work for i = 0?
+
+  let ub' = ub profits weights solP solW (i + 1)
+  if fromIntegral bnd >= ub' then
+    return PruneLevel
+  else
+    return NoPrune
+
+  where
+    -- TODO: Scope capturing function
+    ub :: Array Int Int -> Array Int Int -> Int -> Int -> Item -> Double
+    ub profits weights p w i
+      | i > mix = fromIntegral p
+      | cap - (w + (weights ! i)) >= 0 = ub profits weights (p + (profits ! i)) (w + (weights ! i)) (i + 1)
+      | otherwise = fromIntegral p + (fromIntegral (cap - w) * divd (profits ! i) (weights ! i))
+
+    divd :: Int -> Int -> Double
+    divd a b = fromIntegral a / fromIntegral b
+
+strengthen :: KPNode -> Int -> Bool
+strengthen (_, lbnd, _) gbnd = lbnd > gbnd
+
+--------------------------------------------------------------------------------
 -- Closure Instances
 --------------------------------------------------------------------------------
-instance ToClosure (BAndBFunctions Solution Int Item [Item]) where
+instance ToClosure (BAndBFunctions Solution Int [Item]) where
   locToClosure = $(here)
 
-instance ToClosure (ToCFns Solution Int Item [Item]) where
+instance ToClosure (ToCFns Solution Int [Item]) where
   locToClosure = $(here)
 
 --------------------------------------------------------------------------------
@@ -251,32 +282,36 @@ toClosureInt x = $(mkClosure [| toClosureInt_abs x |])
 toClosureInt_abs :: Int -> Thunk Int
 toClosureInt_abs x = Thunk x
 
+toClosureKPNode :: KPNode -> Closure KPNode
+toClosureKPNode x = $(mkClosure [| toClosureKPNode_abs x |])
+
+toClosureKPNode_abs :: KPNode -> Thunk KPNode
+toClosureKPNode_abs x = Thunk x
+
 
 $(return [])
 declareStatic :: StaticDecl
 declareStatic = mconcat
   [
-    declare (staticToClosure :: StaticToClosure (BAndBFunctions Solution Int Item [Item]))
-  , declare (staticToClosure :: StaticToClosure (ToCFns Solution Int Item [Item]))
+    declare (staticToClosure :: StaticToClosure (BAndBFunctions Solution Int [Item]))
+  , declare (staticToClosure :: StaticToClosure (ToCFns Solution Int [Item]))
 
   -- B&B Functions
-  , declare $(static 'generateChoices)
-  , declare $(static 'shouldPrune)
-  , declare $(static 'shouldUpdateBound)
-  , declare $(static 'step)
-  , declare $(static 'removeChoice)
+  , declare $(static 'orderedGenerator)
+  , declare $(static 'pruningPredicate)
+  , declare $(static 'strengthen)
 
   -- Explicit toClosure
   , declare $(static 'toClosureInteger)
   , declare $(static 'toClosureInteger_abs)
   , declare $(static 'toClosureInt)
   , declare $(static 'toClosureInt_abs)
-  , declare $(static 'toClosureItem)
-  , declare $(static 'toClosureItem_abs)
   , declare $(static 'toClosureItemList)
   , declare $(static 'toClosureItemList_abs)
   , declare $(static 'toClosureSolution)
   , declare $(static 'toClosureSolution_abs)
+  , declare $(static 'toClosureKPNode)
+  , declare $(static 'toClosureKPNode_abs)
 
   , Safe.declareStatic
   ]
