03c5aa7a747edd848aec432e2649108df208b8d7
Blair Archibald <mrblairarchibald@gmail.com>
2016-10-28 11:51:28 +0100

    Add type outline for TSP

--- apps/tsp/src/Main.hs
+++ apps/tsp/src/Main.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
+
+import qualified Bones.Skeletons.BranchAndBound.HdpH.Safe as Safe
+import           Bones.Skeletons.BranchAndBound.HdpH.Types ( BAndBFunctions(BAndBFunctions)
+                                                           , PruneType(..), ToCFns(..))
+import           Bones.Skeletons.BranchAndBound.HdpH.GlobalRegistry
+
+import           Control.Parallel.HdpH
+import qualified Control.Parallel.HdpH    as HdpH (declareStatic)
 
 import Data.Array.Unboxed
 
@@ -29,6 +40,13 @@ optionParser = Options
 
 optsParser :: ParserInfo Options
 optsParser = info (helper <*> optionParser) (fullDesc <> progDesc "Solve TSP" <> header "TSP")
+
+parseHdpHOpts :: [String] -> IO (RTSConf, Int, [String])
+parseHdpHOpts args = do
+  either_conf <- updateConf args defaultRTSConf
+  case either_conf of
+    Left err_msg             -> error $ "parseHdpHOpts: " ++ err_msg
+    Right (conf, [])         -> return (conf, 0, [])
 
 -- Simple TSPLib Parser
 -- FIXME: This might need to be real rather than Int
@@ -83,6 +101,81 @@ buildDistanceMatrix nodes = array ((minId, minId), (maxId, maxId)) distances
         distanceBetween n1 n2 =
           let d = calcDistanceEUC2D n1 n2 in ((nodeInfo_id n1, nodeInfo_id n2), d)
 
+-- Skeleton Functions
+-- SearchNode :: Sol, Bound, Space
+type Location = Int
+type Path = [Location]
+type Solution = (Path, Int)
+
+type SearchNode = (Solution, Int, [Location])
+
+orderedGenerator :: SearchNode -> Par [Par SearchNode]
+orderedGenerator = undefined
+
+pruningPredicate :: SearchNode -> Int -> Par PruneType
+pruningPredicate = undefined
+
+strengthen       :: SearchNode -> Int -> Bool
+strengthen       = undefined
+
+-- Other closury stuff
+orderedSearch :: DistanceMatrix -> Int -> Bool -> Par Path
+orderedSearch distancs depth dds = do
+  (path, _) <- Safe.search
+      dds
+      depth
+      (([],0), 0, [])
+      (toClosure (BAndBFunctions
+                  $(mkClosure [| orderedGenerator |])
+                  $(mkClosure [| pruningPredicate |])
+                  $(mkClosure [| strengthen |])))
+      (toClosure (ToCFns
+                  $(mkClosure [| toClosureSol |])
+                  $(mkClosure [| toClosureInt |])
+                  $(mkClosure [| toClosureLocationList |])
+                  $(mkClosure [| toClosureSearchNode |])))
+
+  return path
+
+-- Explicit toClosure Instances for performance
+toClosureInt :: Int -> Closure Int
+toClosureInt x = $(mkClosure [| toClosureInt_abs x |])
+
+toClosureInt_abs :: Int -> Thunk Int
+toClosureInt_abs x = Thunk x
+
+toClosureSol :: Solution -> Closure Solution
+toClosureSol x = $(mkClosure [| toClosureSol_abs x |])
+
+toClosureSol_abs :: Solution -> Thunk Solution
+toClosureSol_abs x = Thunk x
+
+toClosureLocationList :: [Location] -> Closure [Location]
+toClosureLocationList x = $(mkClosure [| toClosureLocationList_abs x |])
+
+toClosureLocationList_abs :: [Location] -> Thunk [Location]
+toClosureLocationList_abs x = Thunk x
+
+toClosureSearchNode :: SearchNode -> Closure SearchNode
+toClosureSearchNode x = $(mkClosure [| toClosureSearchNode_abs x |])
+
+toClosureSearchNode_abs :: SearchNode -> Thunk SearchNode
+toClosureSearchNode_abs x = Thunk x
+
+instance ToClosure (BAndBFunctions (Path,Int) Int [Location]) where
+  locToClosure = $(here)
+
+instance ToClosure (ToCFns (Path,Int) Int [Location]) where
+  locToClosure = $(here)
+
+$(return []) -- Bring all types into scope for TH.
+
+declareStatic :: StaticDecl
+declareStatic = mconcat
+  [
+    HdpH.declareStatic
+  , Safe.declareStatic
+  ]
 
 main :: IO ()
 main = do
